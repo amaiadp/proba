4d534a0a6cb21a3d09838d2f6d82970fd3877ffd
Amaia <amitxu96@gmail.com>
2016-09-28 10:03:55 +0200

    prueba2 karpeta ezabatu eta hauteskundeetan mahai berri bat gehitu, nire izenarekin

--- hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
+++ hauteskundeak/src/com/isad/bertsioak/Hauteskundeak.java
@@ -24,6 +24,7 @@ public class Hauteskundeak {
 		h2012.gehituMahaia("Irun");
 		h2012.gehituMahaia("Donostia");
 		h2012.gehituMahaia("Tolosa");
+		h2012.gehituMahaia("Amaia de Pablo");
 		
 		System.out.println(h2012);
 	}
